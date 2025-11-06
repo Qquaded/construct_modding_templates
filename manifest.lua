497e332bdae72bf5663a9a87adae22aabf374161
devcell <153192057+Qquaded@users.noreply.github.com>
2025-11-06 23:21:18 +0300

    manifest.lua

---
+++ manifest.lua
@@ -0,0 +1,9 @@
+return {
+  	Name = "ModName",
+    Version = "1.0.0",
+    Author = "devcell",
+	  Description = "a Mod description.",
+	  Thumbnail = "rbxassetid://123456789", --> optional
+  	Icon = "rbxassetid://123456789", --> optional
+    GithubLink = "https://github.com/.../modmodel.rbxm"
+}
